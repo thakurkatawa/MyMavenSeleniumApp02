ea12151bb01b0387857079db1fdd6c623f8d6650
thakurkatawa <thakurkatwa0@gmail.com>
2026-03-05 13:03:27 +0530

    This is Second Login page Testing

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+
+        driver.findElement(By.id("submit")).click();
+
+        System.out.println("Login test executed successfully");
+
+    }
+}
